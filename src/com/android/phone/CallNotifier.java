37b58d9ee9327a88e503660bebe714087d683aef
HardCorePawn <jared.yates@gmail.com>
2013-07-16 06:49:36 +0200

    Phone: Incoming Call in Background
    Allows user to set that Incoming Call UI will remain in background
    if screen is on. Instead, a notification is added to StatusBar, which
    can be used to bring up the Incoming Call UI. Ringtone and vibration
    still occur as per system settings.
    
    This has the same effect as apps marked as "immersive". It prevents
    the incoming call from backgrounding the current activity.
    
    PS 2: minor fix
    
    Change-Id: Ib98dd74268826d511ce43ac75dd9dd66f774c9e7

--- src/com/android/phone/CallNotifier.java
+++ src/com/android/phone/CallNotifier.java
@@ -53,6 +53,7 @@ import android.net.Uri;
 import android.os.AsyncResult;
 import android.os.Handler;
 import android.os.Message;
+import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.SystemProperties;
 import android.os.SystemVibrator;
@@ -203,6 +204,7 @@ public class CallNotifier extends Handler
 
     // Cached system services
     private AudioManager mAudioManager;
+    private PowerManager mPowerManager;
     private Vibrator mVibrator;
 
     // Blacklist handling
@@ -234,6 +236,7 @@ public class CallNotifier extends Handler
         mWaitingCalls = new HashSet<Connection>();
 
         mAudioManager = (AudioManager) mApplication.getSystemService(Context.AUDIO_SERVICE);
+        mPowerManager = (PowerManager) mApplication.getSystemService(Context.POWER_SERVICE);
         mVibrator = (Vibrator) mApplication.getSystemService(Context.VIBRATOR_SERVICE);
 
         registerForNotifications();
@@ -805,6 +808,9 @@ public class CallNotifier extends Handler
             ActivityManagerNative.getDefault().closeSystemDialogs("call");
         } catch (RemoteException e) {
         }
+
+        // Let notificationMgr know the original state of the screen
+        mApplication.notificationMgr.setScreenStateAtIncomingCall(mPowerManager.isScreenOn());
 
         // Go directly to the in-call screen.
         // (No need to do anything special if we're already on the in-call
